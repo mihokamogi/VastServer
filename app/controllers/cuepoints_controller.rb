69ce3952d9e2630368ece10e32f3b57774c61e8e
mogimogi <mocomocomocoo0207@gmail.com>
2017-06-22 09:27:54 +0000

    cuepoint登録作成

--- app/controllers/cuepoints_controller.rb
+++ app/controllers/cuepoints_controller.rb
@@ -7,10 +7,21 @@ class CuepointsController < ApplicationController
   end
 
   def new
+    @cuepoint = Cuepoint.new
   end
 
   def create
+    @cuepoint = Cuepoint.new(cuepoint_params)
+    
+    if @cuepoint.save
+      flash[:success] = "キューポイントを登録しました。"
+      redirect_to root_path
+    else
+      flash[:danger] = "キューポイントの登録に失敗しました。"
+      render :new
+    end
   end
+  
 
   def edit
   end
@@ -20,4 +31,14 @@ class CuepointsController < ApplicationController
 
   def destroy
   end
-end
+
+
+  private
+  
+  def cuepoint_params
+    params.require(:cuepoint).permit(:name)
+  end
+  
+end  
+
+
