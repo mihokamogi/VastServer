18ced86d83119bbb0e918e76af7073b9cf67d528
mogimogi <mocomocomocoo0207@gmail.com>
2017-06-24 10:50:38 +0000

    アソシエーション追加

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,6 @@
 class Campaign < ApplicationRecord
   has_and_belongs_to_many :cuepoints
+  has_many :results
   
   validates :name, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
   validates :limit_start, numericality: { greater_than: 0 , less_than: 10000}
--- app/models/cuepoint.rb
+++ app/models/cuepoint.rb
@@ -5,5 +5,6 @@ class Cuepoint < ApplicationRecord
   has_and_belongs_to_many :campaigns
   
   has_many :campaigns
+  has_many :results
   
 end
