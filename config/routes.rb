4ad454299f7090506aae032092b7056bc2c324e5
mogimogi <mocomocomocoo0207@gmail.com>
2017-06-24 10:46:18 +0000

    ルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ root to: 'campaigns#index'
 get "cuepoints/:id/campaigns" , to: "campaigns#index" 
 post "results?campaign=:id&cuepoint=:id&event=start", to: "results#create"
 post "results?campaign=:id&cuepoint=:id&event=complete", to: "results#create"
-get "cuepoints/:id", to:"cuepoints#edit"
+get "/cuepoints/:id", to: "cuepoints#edit"
 
-resources:cuepoints
-resources:campaigns
+resources:cuepoints, except: :show
+resources:campaigns, except: :show
 resources :results, only: :index 
+
+
 
 end
 
