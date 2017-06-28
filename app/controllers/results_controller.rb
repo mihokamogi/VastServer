7cec119983e48e70b2309b200908adb0a663bbaa
mogimogi <mocomocomocoo0207@gmail.com>
2017-06-28 09:22:55 +0000

    Result新規作成

---
+++ app/controllers/results_controller.rb
@@ -0,0 +1,12 @@
+class ResultsController < ApplicationController
+  
+  def index
+    @results = Result.all.page(params[:page]).per(10)
+  end  
+   
+  def record
+    send_data(Base64.decode64('R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=='),
+type: 'image/gif', disposition: 'inline')
+  end
+  
+end
