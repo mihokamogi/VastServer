89750b1ac7e6bff26adb2d278637f6521adbdcdb
mogimogi <mocomocomocoo0207@gmail.com>
2017-06-30 09:04:28 +0000

    validation変更

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -5,8 +5,21 @@ class Campaign < ApplicationRecord
 
   validates :name, length: { maximum: 50 }, uniqueness: { case_sensitive: false }
   validates :limit_start, numericality: { greater_than: 0 , less_than: 10000}
-  validates :movie_url,length: {maximum: 100 }
-  
+  validates :movie_url,length: { minimum:5, maximum: 100 }
+  validate :end_at_must_be_future
 
+  def end_at_must_be_future
+    if start_at >= end_at
+      errors.add(:end_at, 'error!')
+    end
+  end
+
+  def self.current_available(cuepoint)
+      campaigns = cuepoint.campaigns.where("start_at <= '#{Time.now}' AND end_at >= '#{Time.now}'").to_a
+      campaigns.delete_if do |campaign|
+          result = Result.where(campaign: campaign, cuepoint: @cuepoint).first
+          !result.blank? && result.count_start >= campaign.limit_start 
+      end
+  end
   
 end
