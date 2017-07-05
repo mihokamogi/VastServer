6329b9b4173a7972359e36263c14439cc87e86fd
mogimogi <mocomocomocoo0207@gmail.com>
2017-07-05 04:41:58 +0000

    resultのルート変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,18 +2,15 @@ Rails.application.routes.draw do
 
 root to: 'campaigns#index'
 
+get "results/record"
 
-post "results?campaign=:id&cuepoint=:id&event=start", to: "results#create"
-post "results?campaign=:id&cuepoint=:id&event=complete", to: "results#create"
-get 'signup', to: 'users#new'
- 
+
 resources :cuepoints do
     resources :campaigns, only: [:index]
 end
 
-resources:cuepoints, except: :show
 resources:campaigns, except: :show
-resources :results, only: :index 
+resources :results, only: [:index]
 
 
 
